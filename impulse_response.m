8e7e7d6f512d2f43821837f47bc41a3d1f1a3266
JoWong <jowong@g.hmc.edu>
2016-09-18 19:12:56 -0700

    Edited impulse_response.m to correct equation for h(t) and added plot

--- impulse_response.m
+++ impulse_response.m
@@ -5,11 +5,13 @@ function [ h,t ] = impulse_response( x, y, fs )
 %           y, measured output signal
 %           fs, sampling frequency of measurements
 %   outputs:    h, calculated impulse response
+%edited by Josephine Wong, 09/18/2016
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
-H = X./Y;   % the calculated frequency response
+H = Y./X;   % the calculated frequency response
 [h,t] = tdomain(H,fs);
+plot(t,h)
 
 end
 
